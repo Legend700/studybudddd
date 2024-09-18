60902f5dfc4e7eb50407f83e36aa1d3095764c8c
Legend700 <abdullahbinejaz7@gmail.com>
2024-09-17 21:01:46 -0400

    done

--- django-project-master/base/models.py
+++ django-project-master/base/models.py
@@ -15,7 +15,8 @@ class Room(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    #participants = 
+    participants = models.ManyToManyField(
+        User, related_name='particpants', blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
@@ -33,5 +34,9 @@ class Message(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-updated', '-created']
+
     def __str__(self):
         return self.body[0:50]
+    
